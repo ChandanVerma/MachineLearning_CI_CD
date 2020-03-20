3d2913ef5b58083a47dd4510e10cdeb017999309
Chandan Verma <verma.chandan01@gmail.com>
2020-03-20 13:43:53 +0530

    adding config deatils

--- scripts/data_analysis.py
+++ scripts/data_analysis.py
@@ -7,3 +7,11 @@ from pathlib import Path
 import pandas as pd 
 import numpy as np 
 import matplotlib.pyplot as plt 
+import seaborn as sns
+
+import yaml
+
+with open('config.yaml', 'r') as f:
+    config = yaml.load(f)
+
+config['PATH']['PROJECT_PATH']
