78e27267405b586e84a80e0033d61da799bda468
Chandan Verma <verma.chandan01@gmail.com>
2020-04-15 11:45:58 +0530

    changes in ml_āpi

---
+++ packages/ml_api/api/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from api.controller import prediction_app
+def create_app() -> Flask:
+
+    flask_app = Flask('ml_api')
+
+    from api.controller import prediction_app
+    flask_app.register_blueprint(prediction_app)
+
+    return flask_app
